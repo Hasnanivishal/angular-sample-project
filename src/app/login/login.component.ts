1085ae8bde97b012b2bfae37528f4658cc8d009d
vishal.hasnani.rtpl <vishal.hasnani@ranosys.com>
2019-06-25 14:12:55 +0530

    created unit test cases for login component.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -27,7 +27,8 @@ export class LoginComponent implements OnInit {
       // send a http request to save this data
       this.guestUserService.login(this.loginForm.value).subscribe(
         result => {
-          if (result['token']) {
+          if (result) {
+          // if (result['token']) {
             localStorage.setItem('authToken', result['token']);
             this.router.navigate(['/dashboard']);
           }
