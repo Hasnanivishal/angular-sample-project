a422fd3f9112753c5a968e87710886aa973296bf
vishal.hasnani.rtpl <vishal.hasnani@ranosys.com>
2019-07-05 13:23:06 +0530

    Updated test cases for the error interceptor.

--- src/app/service/error-interceptor.service.spec.ts
+++ src/app/service/error-interceptor.service.spec.ts
@@ -1,12 +1,35 @@
 // import { TestBed } from '@angular/core/testing';
+// import { ErrorInterceptor } from './error-interceptor.service';
+// import { HttpClientTestingModule } from '@angular/common/http/testing';
+// import { HTTP_INTERCEPTORS, HttpRequest } from '@angular/common/http';
 
-// import { ErrorInterceptorService } from './error-interceptor.service';
 
-// describe('ErrorInterceptorService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+// fdescribe('ErrorInterceptorService', () => {
 
-//   it('should be created', () => {
-//     const service: ErrorInterceptorService = TestBed.get(ErrorInterceptorService);
-//     expect(service).toBeTruthy();
-//   });
+//     let errorInterceptor: ErrorInterceptor;
+
+//     beforeEach(() => TestBed.configureTestingModule({
+//         imports: [HttpClientTestingModule],
+//         providers: [ErrorInterceptor,
+//             {
+//                 provide: HTTP_INTERCEPTORS,
+//                 useClass: ErrorInterceptor,
+//                 multi: true
+//             }]
+//     }));
+
+//     it('should be created', () => {
+//         errorInterceptor = TestBed.get(ErrorInterceptor);
+//         expect(errorInterceptor).toBeTruthy();
+//     });
+
+//     it('should remove request from HttpRequestArray', () => {
+//         errorInterceptor.requests = [ new HttpRequest<any>('GET', '/'), new HttpRequest<any>('POST', '/', null),
+//         new HttpRequest<any>('GET', '/'), new HttpRequest<any>('GET', '/') ];
+
+//         errorInterceptor.removeRequest(new HttpRequest<any>('POST', '/', null));
+
+//         expect(errorInterceptor.requests).not.toContain(new HttpRequest<any>('POST', '/', null));
+
+//     });
 // });
