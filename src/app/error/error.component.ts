54567dc53c7d0ad5b15e4fb21cc33699d64386de
vishal.hasnani.rtpl <vishal.hasnani@ranosys.com>
2019-07-19 11:02:56 +0530

    Updated error page.

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -7,10 +7,4 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent {
   constructor() { }
-  htmlValue: any;
-
-  onValueChange(event: { markDown: any; html: any; }) {
-    console.log(event.markDown); // this should print markdown which user entered
-    console.log(event.html); // this should print html output for the markdown
-  }
 }
